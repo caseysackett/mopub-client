df7cd99920d35b89856d5326a5d9f17571bf4cc4
Casey Sackett <csackett@gmail.com>
2013-03-16 14:54:30 -0700

    use the 600x90 banner on Kindle Fire HD 7" even though it needs to scroll a little.  it still looks better than 320x50.

--- Android/mopub-android-sdk/src/com/mopub/mobileads/AmazonBanner.java
+++ Android/mopub-android-sdk/src/com/mopub/mobileads/AmazonBanner.java
@@ -48,6 +48,7 @@ public class AmazonBanner extends CustomEventBanner implements AdListener {
         int widthPixels = context.getResources().getDisplayMetrics().widthPixels;
         AdLayout.AdSize adSize = AdLayout.AdSize.AD_SIZE_320x50;
         if(widthPixels == 600) adSize = AdLayout.AdSize.AD_SIZE_600x90; 	// Kindle Fire
+        if(widthPixels == 800) adSize = AdLayout.AdSize.AD_SIZE_600x90; 	// Kindle Fire HD 7"
         if(widthPixels == 728) adSize = AdLayout.AdSize.AD_SIZE_728x90;		// Only if it matches exactly, since 728 scrolls on 800px wide Kindle Fire HD 7"
         if(widthPixels >= 1024) adSize = AdLayout.AdSize.AD_SIZE_1024x50; 	// Kindle Fire HD
         
